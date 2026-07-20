c07e5be64491a213e14f9bf39097cffcb50a91eb
CaioNatividade <caioprofissional22@gmail.com>
2026-07-20 12:21:39 -0300

    feat: implementa estrutura base MVC e Front Controller inicial

---
+++ app/views/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Rastreador TI</title>
+</head>
+
+<body>
+  <h1>Bem-vindo ao Sistema</h1>
+  <p>Primeira página utilizando MVC.</p>
+</body>
+
+</html>
--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
+require_once '../app/controllers/HomeController.php';
 
-echo "Olá, Mundo";
+$rota = $_GET['rota'] ?? 'home';
 
-?>
+switch ($rota) {
+  case 'home':
+    $controller = new HomeController();
+    $controller->index();
+    break;
+  default:
+    echo "Página não encontrada";
+}
